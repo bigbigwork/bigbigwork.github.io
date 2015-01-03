d1275e7bbf90c8320feb933a22cabf80f070222e
bigbigwork <bigbigwork@outlook.com>
2015-01-03 13:20:31 +0800

    。。。

--- bigGame/app.js
+++ bigGame/app.js
@@ -4,13 +4,22 @@
 _.ready(function(){
     /**/
     var i=0;
-    _.initCanvas("canvas_box");
-    _.set_looping(function(){
-        _.clear_canvas();
-        _.c.font="50px Georgia";
-        _.c.fillStyle="#f00";
-        _.c.fillText(i++,10,60);
-        //_("#debug").text(i++);
-    });
-    _.animate();
+    _.initCanvas("canvas_box",500,500);
+
+
+    var screen=new Screen();
+    var time_text=new Text("time",60,60);
+    time_text.setText(1);
+    time_text.setColor("#f00");
+    time_text.setUpdate(function(){
+        this.setText(this.getText()+1);
+        //console.log(this.getText());
+        //_.c.font="50px Georgia";
+        //_.c.fillStyle="#f00";
+        //_.c.fillText(this.getText(),this.x,this.y);
+    })
+    screen.add(time_text);
+
+    var render=new Render();
+    render.render(screen,1000);
 });
--- bigbigwork2d.js
+++ bigbigwork2d.js
@@ -2,12 +2,12 @@
  * Created by g on 2015/1/1.
  * 2d 引擎
  */
-_.initCanvas=function(id){
+_.initCanvas=function(id,w,h){
     var canvas_box=document.querySelector("#"+id);
     var canvas=document.createElement("canvas");
     //
-    _.canvas_w=300;
-    _.canvas_h=300;
+    _.canvas_w=w;
+    _.canvas_h=h;
     canvas.width=_.canvas_w;
     canvas.height=_.canvas_h;
     canvas.top = 0;
@@ -16,14 +16,106 @@ _.initCanvas=function(id){
     canvas_box.appendChild(canvas);
     //
 }
+_.engine=function(){
+
+}
+_.create_scene=function(){
+
+}
+_.sprites=new Array();
+
+
+var Sprite=function(name,x,y,src){
+
+}
+Sprite.update=function(){
+
+}
+_.create_sprite=function(name,x,y,src){
+
+}
 _.clear_canvas=function(){
     _.c.clearRect(0, 0, _.canvas_w, _.canvas_h);
 }
 _.set_looping=function(x){
     _.looping=x;
 }
+_.setAniTime=function(t){
+    _.anitime=t;
+}
+_.lastT=0;
 _.animate=function() {
-    //console.log("animate");
+    var t=new Date().getTime();
+    if(t- _.anitime> _.lastT){
+        _.lastT=t;
+        _.looping();
+    }
     requestAnimationFrame(_.animate );
-    _.looping();
+    //console.log(t- _.anitime)
+
+}
+
+_.screen=function(){
+
+}
+
+var Screen=function(){
+    this.objs=new Array();
+}
+Screen.prototype.add=function(obj){
+    this.objs.push(obj);
+}
+
+
+
+var Render=function(){
+    this.animate=function(){
+        var loop=function(objs,fps) {
+            var t=new Date().getTime();
+            if(t-fps> _.lastT){
+                _.lastT=t;
+                _.clear_canvas();
+                for(i=0;i<objs.length;i++){
+                    objs[i].update();
+                    //console.log(objs[i])
+                }
+            }
+            requestAnimationFrame(function(){
+                loop(objs,fps);
+            });
+        }
+        loop(this.screen.objs,this.fps);
+    }
+}
+Render.prototype.render=function(screen,fps){
+    this.screen=screen;
+    var this_time=new Date().getTime();
+    this.fps=fps;
+    this.animate();
+
+}
+
+var Text=function(id,x,y){
+    this.id=id;
+    this.x=x;
+    this.y=y;
+    this.color="#fff";
+}
+Text.prototype.update=function(){
+    this.updateExec();
+    _.c.font="50px Georgia";
+    _.c.fillStyle=this.color;
+    _.c.fillText(this.getText(),this.x,this.y);
+}
+Text.prototype.setColor=function(color){
+    this.color=color;
+}
+Text.prototype.setText=function(text){
+    this.text=text;
+}
+Text.prototype.getText=function(){
+    return this.text;
+}
+Text.prototype.setUpdate=function(x){
+    this.updateExec=x;
 }
