66f9ee976819d6ff04fa9a6469226cc31f7bd09c
bigbigwork <bigbigwork@outlook.com>
2015-03-19 11:19:37 +0800

    index

---
+++ js/app.js
@@ -0,0 +1,26 @@
+/**
+ * Created by js on 2015/3/19.
+ */
+window.onload=function(){
+    var login_value=0;
+    document.getElementById("res").innerHTML="";
+    function loading(){
+        login_value++;
+        var e=document.createElement("div");
+        e.className="loading_add";
+        document.getElementById("res").appendChild(e);
+        setTimeout(function(){
+            if(login_value===100){
+                document.getElementById("loader").className="end";
+                setTimeout(function(){
+                    document.getElementById("res").innerHTML="";
+                    document.getElementById("loader").className="hide";
+                },1500);
+
+            }else{
+                loading();
+            }
+        },20);
+    }
+    loading();
+}
