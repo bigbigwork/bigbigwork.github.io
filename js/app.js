926024b97f63b7e1508e1a95a372d2686fb195cf
bigbigwork <bigbigwork@outlook.com>
2015-03-19 12:33:32 +0800

    。。。

--- js/app.js
+++ js/app.js
@@ -1,6 +1,20 @@
 /**
  * Created by js on 2015/3/19.
  */
+function include_js(path) {
+    var sobj = document.createElement('script');
+    sobj.type = "text/javascript";
+    sobj.src = path;
+    var headobj = document.getElementsByTagName('head')[0];
+    headobj.appendChild(sobj);
+}
+
+function include_css(path){
+    var fileref=document.createElement("link")
+    fileref.rel = "stylesheet";
+    fileref.type = "text/css";
+    fileref.href = path;
+}
 window.onload=function(){
     var login_value=0;
     document.getElementById("res").innerHTML="";
@@ -14,7 +28,10 @@ window.onload=function(){
                 document.getElementById("loader").className="end";
                 setTimeout(function(){
                     document.getElementById("res").innerHTML="";
+                    document.getElementById("works").className="imgList";
                     document.getElementById("loader").className="hide";
+                    include_js("js/photo.js");
+
                 },1500);
 
             }else{
