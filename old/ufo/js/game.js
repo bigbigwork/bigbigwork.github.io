da33969888537b89f1dcb91d02589d7f9c68114a
bigbigwork <bigbigwork@outlook.com>
2015-03-24 22:35:33 +0800

    ...

---
+++ old/ufo/js/game.js
@@ -0,0 +1,50 @@
+function Game(){
+	this.imageGameOver='';
+	this.imageUFO='';
+	this.imageFire='';
+	this.imageMe='';
+	
+	this.v=0;
+	this.state='playing';
+	this.x=10;
+	this.y=150;
+}
+Game.prototype.showv=function(){
+	ctx.font="30px Georgia";
+	ctx.fillText(game.v,30,30);
+}
+Game.prototype.setImageGameOver=function(imageGameOver){
+	this.imageGameOver=imageGameOver;
+}
+Game.prototype.setImageUFO=function(imageUFO){
+	
+	this.imageUFO=imageUFO;
+		
+}
+Game.prototype.setImageFire=function(imageFire){
+	this.imageFire=imageFire;
+}
+Game.prototype.setImageMe=function(imageMe){
+	this.imageMe=imageMe;
+
+
+}
+Game.prototype.start=function(){
+	//ctx.globalCompositeOperation="xor";
+	/*animate(function(){
+		rendering();
+	});*/
+	setInterval(rendering, 1000 / 24);
+}
+Game.prototype.gameover=function(){
+	this.state='gameover';
+	ctx.globalCompositeOperation="source-over";
+	document.getElementById("pop_game_over").style.display="block";
+	//ctx.drawImage(this.imageGameOver,this.x,this.y);
+}
+Game.prototype.pause=function(){
+	this.state='pause';
+}
+Game.prototype.play=function(){
+	this.state='playing';
+}
